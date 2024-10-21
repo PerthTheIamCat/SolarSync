4bc313e8c510803874b2abdaa322831e45a2a23d
PerthTheIamCat <thongcum2546@gmail.com>
2024-10-22 02:38:17 +0700

    add sidebar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,24 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { useNavigate } from "react-router-dom";
 
 function Navbar({ isTokenValid }) {
+  const [isProfileOpen, setIsProfileOpen] = useState(false);
+  const openProfile = () => {
+    console.log("Open Profile");
+    setIsProfileOpen(!isProfileOpen);
+  };
+
   const navigate = useNavigate();
   return (
     <>
+      <div id="side-bar" className={isProfileOpen ? "open" : ""} >
+        <div id="use-zone">
+          <h2>Profile</h2>
+          <h2>Settings</h2>
+          <h2 onClick={()=>{ window.localStorage.removeItem("token"); navigate("/signin"); }}>Log Out</h2>
+        </div>
+      </div>
       <div className="container">
         <ul>
           <h1 className="LOGO" onClick={()=>{navigate("/")}}>LOGO</h1>
           {isTokenValid ? (
-            <h1
-              className="signout"
-              onClick={() => {
-                localStorage.removeItem("token");
-                navigate("/signin");
-              }}
-            >
-              Sign out
-            </h1>
+            <div>
+              <div id="profile" onClick={openProfile}>
+                <img src="https://popcat.click/twitter-card.jpg" alt="profile" />
+              </div>
+            </div>
           ) : window.location.pathname === "/signup" ? (
             <h1
               className="signup"
