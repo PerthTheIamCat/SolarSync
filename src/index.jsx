828f3d7e77a65adac6d83b3739cd33d9aec91095
PerthTheIamCat <thongcum2546@gmail.com>
2024-10-21 18:27:47 +0700

    Refactor file extensions to .jsx for App.js and index.js

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App.jsx';
 import reportWebVitals from './reportWebVitals';
-import Navbar from './Navbar';
+import Navbar from './Navbar.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
