bc0caed6fe11d557cd9a4f7e06cebff4ee075a6c
PerthTheIamCat <thongcum2546@gmail.com>
2024-10-22 01:13:27 +0700

    use token to update page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,25 +1,69 @@
-import React from 'react';
-import './Home.css';
-import { useNavigate } from 'react-router-dom'
-import Navbar from './Navbar';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
+import Navbar from "./Navbar";
+import "./Home.css";
+import Loading from "./Loading";
 
 function Home() {
-    document.title = "Home";
-    const navigate = useNavigate();
-    return ( 
-        <div id="home-container">
-            <Navbar/>
-            <div className="HOME relative">
-                <div className="banner"></div>
-                <h1>LOGO</h1>
-                <button className="sign-in-btn" onClick={()=>navigate("/signIn")}>Sign In</button>
-            </div> 
-            <div className="HOME">
-                <h1>Home</h1>
-                <p>Welcome to the Home page.</p>
-            </div>
-        </div>
-    );
+  document.title = "Home";
+  const navigate = useNavigate();
+  const [isTokenValid, setIsTokenValid] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const checkToken = async () => {
+      const token = localStorage.getItem("token");
+      setIsLoaded(false);
+      if (token) {
+      try {
+        const decodedToken = await jwtDecode(token);
+        const currentTime = Date.now() / 1000;
+        if (decodedToken.exp > currentTime) {
+        console.log("Token is valid, user is logged in.");
+        setIsTokenValid(true);
+        } else {
+        console.log("Token has expired.");
+        localStorage.removeItem("token");
+        setIsTokenValid(false);
+        navigate("/signIn");
+        }
+      } catch (error) {
+        console.error("Invalid token format", error);
+        localStorage.removeItem("token");
+        setIsTokenValid(false);
+        navigate("/signIn");
+      }
+      } else {
+      console.log("No token found, stay on the Home page.");
+      }
+      setIsLoaded(true);
+    };
+    checkToken();
+  }, [navigate]);
+
+  if (!isLoaded) {
+    return <Loading />;
+  }
+
+  return (
+    <div id="home-container">
+      <Navbar isTokenValid={isTokenValid}/>
+      <div className="HOME relative">
+        <div className="banner"></div>
+        <h1>LOGO</h1>
+        {isTokenValid ? "" : (
+          <button className="sign-in-btn" onClick={() => navigate("/signIn")}>
+            Sign In
+          </button>
+        )}
+      </div>
+      <div className="HOME">
+        <h1>Home</h1>
+        <p>Welcome to the Home page.</p>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,14 +2,24 @@ import React from "react";
 import "./Navbar.css";
 import { useNavigate } from "react-router-dom";
 
-function Navbar() {
+function Navbar({ isTokenValid }) {
   const navigate = useNavigate();
   return (
     <>
       <div className="container">
         <ul>
           <h1 className="LOGO" onClick={()=>{navigate("/")}}>LOGO</h1>
-          {window.location.pathname === "/signup" ? (
+          {isTokenValid ? (
+            <h1
+              className="signout"
+              onClick={() => {
+                localStorage.removeItem("token");
+                navigate("/signin");
+              }}
+            >
+              Sign out
+            </h1>
+          ) : window.location.pathname === "/signup" ? (
             <h1
               className="signup"
               onClick={() => {
