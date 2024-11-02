ace5ac5b7349e0f8a37ac5df642570b3010214ca
phuriphat20 <phuriphat696@gmail.com>
2024-11-03 02:52:54 +0700

    enhance Navbar component with sign-up functionality; add onOpenSignUp prop and update sign-up link behavior

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,26 +1,41 @@
 import React, { useState } from "react";
 import "./Navbar.css";
 import { useNavigate } from "react-router-dom";
+import Signin from "../SignIn/Signin";
+import SignUp from "../SignUp/Signup";
 
-function Navbar({ isTokenValid }) {
+function Navbar({onOpenSignUp:(), isTokenValid}) {
+ 
   const [isProfileOpen, setIsProfileOpen] = useState(false);
+  const navigate = useNavigate();
+  
+  const OpenSignUp = () => {
+    console.log("Open SignUp");
+    onOpenSignUp("");
+  };
   const openProfile = () => {
     console.log("Open Profile");
     setIsProfileOpen(!isProfileOpen);
   };
 
-  const navigate = useNavigate();
+  
+
   return (
     <>
-      <div id="side-bar" className={isProfileOpen ? "open" : ""} >
+      <div id="side-bar" className={isProfileOpen ? "open" : ""}>
         <div id="use-zone">
-          <h2 onClick={()=>{ navigate("/setting")}}>Settings</h2>
-          <h2 onClick={()=>{ window.localStorage.removeItem("token"); navigate("/signin"); }}>Log Out</h2>
+          <h2 onClick={() => navigate("/setting")}>Settings</h2>
+          <h2 onClick={() => { 
+            window.localStorage.removeItem("token"); 
+            navigate("/signin"); 
+          }}>
+            Log Out
+          </h2>
         </div>
       </div>
       <div className="container">
         <ul>
-          <h1 className="LOGO" onClick={()=>{navigate("/")}}>LOGO</h1>
+          <h1 className="LOGO" onClick={() => navigate("/")}>LOGO</h1>
           {isTokenValid ? (
             <div>
               <div id="profile" onClick={openProfile}>
@@ -28,26 +43,15 @@ function Navbar({ isTokenValid }) {
               </div>
             </div>
           ) : window.location.pathname === "/signup" ? (
-            <h1
-              className="signup"
-              onClick={() => {
-                navigate("/signin");
-              }}
-            >
-              Sign in
-            </h1>
+            <h1 className="signin"></h1>
           ) : (
-            <h1
-              className="signin"
-              onClick={() => {
-                navigate("/signup");
-              }}
-            >
-              Sign up
+            <h1 className="signin" onClick={()=> onOpenSignUp()}>
+              Sign Up
             </h1>
           )}
         </ul>
       </div>
+
     </>
   );
 }
